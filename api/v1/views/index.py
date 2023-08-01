25325c67213892d2ee1097045420387842ad9a8a
dismond5 <omarkhaled201148@gmail.com>
2023-08-01 06:27:43 -0700

    a

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """Flask application that retrieves information"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status", strict_slashes=False)
